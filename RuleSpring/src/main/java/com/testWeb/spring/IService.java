31fedc5a47ce003075f1fe04238522f6d34c6184
SirkoOtaman <shychka@dio381.dio-soft.local>
2014-11-24 12:25:10 +0200

    optimize imports

--- RuleSpring/src/main/java/com/testWeb/spring/IService.java
+++ RuleSpring/src/main/java/com/testWeb/spring/IService.java
@@ -1,9 +1,6 @@
 package com.testWeb.spring;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import javax.inject.Named;
 
 /**
  * Created by shychka on 11/20/2014.
