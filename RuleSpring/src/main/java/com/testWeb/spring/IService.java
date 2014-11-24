e72367b28f73fd0b71eeea7b012bfc9fbf80a02b
SirkoOtaman <shychka@dio381.dio-soft.local>
2014-11-24 12:16:11 +0200

    change Service to Named

--- RuleSpring/src/main/java/com/testWeb/spring/IService.java
+++ RuleSpring/src/main/java/com/testWeb/spring/IService.java
@@ -1,14 +1,9 @@
 package com.testWeb.spring;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-import javax.inject.Named;
 
 /**
  * Created by shychka on 11/20/2014.
  */
-@Service
 public interface IService {
     public void doIt();
 }
